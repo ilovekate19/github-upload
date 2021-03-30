ee2cdce3e923f621f486ee8974248376e0905963
ilovekate19 <ilovekate19@gmail.com>
2021-03-30 15:25:13 +0900

    initializing repository

---
+++ src/main/java/com/github/simplestep/kafka/tutorial2/TwitterProducer.java
@@ -0,0 +1,23 @@
+package com.github.simplestep.kafka.tutorial2;
+
+public class TwitterProducer {
+
+    public TwitterProducer(){}
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+
+    }
+
+    public void run() {
+        // create a twitter client
+
+        // create a kafka producer
+
+        //loop to send twttes to kafka
+    }
+
+    public void createTwitterClient(){
+
+    }
+}
